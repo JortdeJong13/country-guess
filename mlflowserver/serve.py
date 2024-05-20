d1859eac771b38cea2af5fc4e7d7274a72b52a35
JortdeJong1399 <jortdejong13@hotmail.nl>
2024-05-20 15:13:52 +0200

    Starting with docker

---
+++ mlflowserver/serve.py
@@ -0,0 +1,11 @@
+from mlflow.pytorch import load_model
+from countryguess.data import Dataset
+
+# Load model
+model = load_model("models:/triplet_model@champion")
+
+# Load reference data 
+model.load_reference(Dataset(shape=model.shape))
+
+# Start inference server
+model.serve(port=5000)
