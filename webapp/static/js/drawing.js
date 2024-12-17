ed5e0001c144766dcb2ab6d0fa90b2ac40079dfd
JortdeJong1399 <04.gazebos.film@icloud.com>
2024-12-17 19:28:30 +0100

    New messages

--- webapp/static/js/drawing.js
+++ webapp/static/js/drawing.js
@@ -10,6 +10,56 @@ canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", stopDrawing);
 
+const initialGuessMessages = [
+  "It looks like you tried to draw {{guessed}}.",
+  "I think this might be {{guessed}}. Am I right?",
+  "Hmm, this looks like {{guessed}} to me.",
+  "If I had to guess, I’d say this is {{guessed}}.",
+  "Is this supposed to be {{guessed}}? That’s my guess!",
+  "This reminds me of {{guessed}}.",
+  "Could it be {{guessed}}? That’s what I’m seeing.",
+  "I’m going with {{guessed}}...",
+  "It’s giving me {{guessed}} vibes. Am I close?",
+  "My best guess? Definitely {{guessed}}.",
+];
+
+const correctMessages = [
+  "I know a {{selected}} when I see one!",
+  "You’ve got it! That’s clearly {{selected}}—nice work!",
+  "No doubt about it, that’s {{selected}}. Good job!",
+  "Ah, {{selected}}! I recognized that right away.",
+  "Great drawing! I knew it was {{selected}} instantly.",
+  "You and I are in sync—that’s {{selected}}!",
+  "I didn’t even have to think twice—it’s {{selected}}!",
+  "That’s a perfect {{selected}}! You nailed it.",
+  "Well done! I couldn’t miss {{selected}} if I tried.",
+  "It’s {{selected}}! Excellent work from both of us!",
+];
+
+const incorrectMessages = [
+  "Hmm, I guessed {{guessed}}, but it’s actually {{selected}}.",
+  "Oops, I thought it was {{guessed}}. It’s {{selected}}—my bad!",
+  "Not quite—I went with {{guessed}}, but it’s really {{selected}}.",
+  "Close one! I guessed {{guessed}}, but this is actually {{selected}}.",
+  "Well, I gave it my best shot! I thought it was {{guessed}}, but it’s {{selected}}.",
+  "Ah, I see it now—it’s {{selected}}, not {{guessed}}. Thanks for clarifying!",
+  "Interesting! I guessed {{guessed}}, but you drew {{selected}}.",
+  "My guess was {{guessed}}, but you were aiming for {{selected}}. Almost there!",
+  "I’ll admit it—I was wrong. This is {{selected}}, not {{guessed}}.",
+  "Oops! I thought {{guessed}}, but you drew a great {{selected}}.",
+];
+
+function getRandomMessage(messageList, guessedCountry, selectedCountry) {
+  // Pick a random message from the list
+  const randomIndex = Math.floor(Math.random() * messageList.length);
+  const template = messageList[randomIndex];
+
+  // Replace placeholders with the given values
+  return template
+    .replace("{{selected}}", selectedCountry)
+    .replace("{{guessed}}", guessedCountry);
+}
+
 function startDrawing(event) {
   isDrawing = true;
   var x = event.clientX - canvas.getBoundingClientRect().left;
@@ -66,8 +116,12 @@ function guess() {
         return response.json();
       })
       .then((data) => {
-        document.getElementById("guess-message").innerText =
-          "It looks like you tried to draw " + data.ranking[0];
+        const message = getRandomMessage(
+          initialGuessMessages,
+          data.ranking[0],
+          data.ranking[0],
+        );
+        document.getElementById("guess-message").innerText = message;
         window.currentDrawingId = data.drawing_id;
         showConfirmation(data.ranking);
       })
@@ -138,8 +192,26 @@ document
 
 function confirmCountry() {
   var selectedCountry = document.getElementById("country-dropdown").value;
-  document.getElementById("guess-message").innerText =
-    "You tried to draw " + selectedCountry;
+  const guessedCountry =
+    document.getElementById("country-dropdown").options[0].value;
+
+  if (selectedCountry === guessedCountry) {
+    const message = getRandomMessage(
+      correctMessages,
+      selectedCountry,
+      guessedCountry,
+    );
+    document.getElementById("guess-message").innerText = message;
+    // Add confetti here
+  } else {
+    const message = getRandomMessage(
+      incorrectMessages,
+      selectedCountry,
+      guessedCountry,
+    );
+    document.getElementById("guess-message").innerText = message;
+  }
+
   hideConfirmation();
 
   // Send POST request to feedback route
