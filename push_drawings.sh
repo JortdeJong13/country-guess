9ebd5b617d46cd3fe182a8d10dd941cfb3fe9930
JortdeJong1399 <jortdejong13@hotmail.nl>
2025-04-19 13:55:26 +0200

    Updated commit message

--- push_drawings.sh
+++ push_drawings.sh
@@ -2,7 +2,7 @@
 
 if git status --porcelain | grep -q "data/drawings/"; then
   git add data/drawings/*
-  git commit -m "New drawings from push_drawings.sh"
+  git commit -m "New drawings"
   git push origin main:drawings
 else
   echo "No new drawings to push."
